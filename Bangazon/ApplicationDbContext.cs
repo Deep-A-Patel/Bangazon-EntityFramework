3c6db624a6d32f3407245ff93e19a7f85852b01a
Deep-A_Patel <deep.ag123007@gmail.com>
2019-08-21 09:16:53 -0700

    second commit

---
+++ Bangazon/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Bangazon.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bangazon.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        //public DbSet<Employee> Employee { get; set; }
+        public DbSet<Department> Department { get; set; }
+    }
+}
